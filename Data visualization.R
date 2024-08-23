83eb29f6b2e20bce70f0b8d4a95536f3e87add51
nimpano <naphtarimpano@gmail.com>
2024-08-23 09:10:46 +0200

    Updated visuals

--- Data visualization.R
+++ Data visualization.R
@@ -1,5 +1,7 @@
 ## Data visualization in basic plot
 
+# BOX PLOT
+
 data(mtcars) # load the mtcars data set 
 summary(mtcars) # data exploration
 str(mtcars) 
@@ -7,3 +9,7 @@ str(mtcars)
 ?boxplot # Get more info on the function
 boxplot(wt~gear, data = mtcars, xlab = "Gear", ylab = "Weight", main = "Box plot
         Relationship between weight and gear in mtcars data set", ylim = c(0,6)) 
+?rep
+male <- rep("male", 1000)
+female <- rep("female", 5000)
+
