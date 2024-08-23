388ce6cffc488d4a757473c94cc7eed28434ccaa
Naphtar Impano <117766310+nimpano@users.noreply.github.com>
2024-08-23 09:22:10 +0200

    Update Data visualization.R

--- Data visualization.R
+++ Data visualization.R
@@ -7,8 +7,12 @@ summary(mtcars) # data exploration
 str(mtcars) 
 
 ?boxplot # Get more info on the function
-boxplot(wt~gear, data = mtcars, xlab = "Gear", ylab = "Weight", main = "Box plot
-        Relationship between weight and gear in mtcars data set", ylim = c(0,6)) 
+boxplot(wt~gear, data = mtcars,
+        xlab = "Gear", 
+        ylab = "Weight", 
+        ylim = c(0,6)
+        main = "Box plot relationship between weight and gear in mtcars data set",
+        ) 
 ?rep
 male <- rep("male", 1000)
 female <- rep("female", 5000)
